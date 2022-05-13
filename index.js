7205e9cf588e915b874b1ee39ae3dae7d2df9097
NickeaBennett <nickeabennett@gmail.com>
2022-05-13 09:54:11 -0400

    Solved 9. Working on the rest

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function createEmployeeRecord(obj){
+    return {
+        firstName: obj[0],
+        familyName: obj[1],
+        title: obj[2],
+        payPerHour: obj[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(object) {
+    return 
+        firstName: "",
+}
