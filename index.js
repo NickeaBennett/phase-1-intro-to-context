a418c754f957b379935dcd431d68cb9db456723e
NickeaBennett <nickeabennett@gmail.com>
2022-05-20 00:14:34 -0400

    Assignment

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
-// Your code here
-function createEmployeeRecord(obj){
-    return {
-        firstName: obj[0],
-        familyName: obj[1],
-        title: obj[2],
-        payPerHour: obj[3],
-        timeInEvents: [],
-        timeOutEvents: []
+const createEmployeeRecord = ([firstName, familyName, title, payPerHour]) => {
+     return {
+        firstName,
+        familyName,
+        title,
+        payPerHour,
+        timeInEvents : [],
+        timeOutEvents : []
     }
 }
 
-function createEmployeeRecords(object) {
-    return 
-        firstName: "",
+const createEmployeeRecords = (employeeRecords) => {
+    const result = []
+    for (const employee of employeeRecords) {
+        result.push(createEmployeeRecord(employee));
+    }
+    return result;
+}
+
+const createTimeInEvent = (record, timeStamp) => {
+
+    const timeObj = {
+        type: "TimeIn",
+        date: timeStamp.split(" ")[0],
+        hour: parseInt(timeStamp.slice(-4), 10)
+    }
+    record.timeInEvents.push(timeObj)
+    return record
+}
+
+const createTimeOutEvent = (record, timeStamp) => {
+
+    const timeObj = {
+        type: "TimeOut",
+        date: timeStamp.split(" ")[0],
+        hour: parseInt(timeStamp.slice(-4), 10)
+    }
+    record.timeOutEvents.push(timeObj)
+    return record
+}
+
+const hoursWorkedOnDate = (record, date) => {
+    const timeIn = record.timeInEvents.find(e => {
+        return e.date === date
+    }).hour
+    const timeOut = record.timeOutEvents.find(e => {
+        return e.date === date
+    }).hour
+    return (timeOut - timeIn) / 100
+}
+
+const wagesEarnedOnDate = (record, date) => {
+    return hoursWorkedOnDate(record, date) * record.payPerHour
+}
+
+const allWagesFor = (record) => {
+    let pay = 0;
+    for (let i = 0; i < record.timeInEvents.length; i++) {
+        let payDay = wagesEarnedOnDate(record, record.timeInEvents[i])
+        pay += payDay
+    }
+    return pay
 }
